4489d0987be764999ea28248744e95ea9a0ec186
Oscar Olotu <oscar.eugine@gmail.com>
2018-02-21 07:54:50 +0300

    Changed the 404 page

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -1,12 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-  @include('partials.page-header')
+  {{--  @include('partials.page-header')  --}}
 
   @if (!have_posts())
-    <div class="alert alert-warning">
-      {{ __('Sorry, but the page you were trying to view does not exist.', 'sage') }}
+    <div class="jumbotron text-center bg-white mb-5">
+      @include('partials.page-header')
+      <p class="lead mb-5">{{ __('Sorry, but the page you were trying to view does not exist.', 'sage') }}.</p>
+      <p class="lead">
+        <a class="btn btn-outline-secondary" href="<?= esc_url(home_url('/')); ?>" role="button">{{ __('Back Home', 'sage') }}</a>
+      </p>
     </div>
-    {!! get_search_form(false) !!}
   @endif
 @endsection
