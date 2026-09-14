c1e88da4f667072c15fbb0bc57e8b1704bb2ab2e
Oscar Olotu <oscar.eugine@gmail.com>
2026-09-14 21:59:14 +0300

    feat(seo): include series and scheduled publishing in sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,13 +1,26 @@
 import { allPosts } from "contentlayer/generated";
 
-export default async function sitemap() {
-  const posts = allPosts.map((post) => ({
-    url: `https://oscarolotu.com/blog/${post.slug}`,
-    lastModified: post.publishedAt,
-  }));
+export const revalidate = 300;
 
-  const routes = ["", "/about", "/projects", "/blog"].map((route) => ({
-    url: `https://oscarolotu.com${route}`,
+const siteUrl = "https://oscar.co.tz";
+
+export default async function sitemap() {
+  const now = new Date();
+  const posts = allPosts
+    .filter((post) => new Date(post.publishedAt) <= now)
+    .map((post) => ({
+      url: `${siteUrl}/blog/${post.slug}`,
+      lastModified: post.updatedAt || post.publishedAt,
+    }));
+
+  const routes = [
+    "",
+    "/about",
+    "/projects",
+    "/blog",
+    "/blog/series/wema-kama-uhalifu",
+  ].map((route) => ({
+    url: `${siteUrl}${route}`,
     lastModified: new Date().toISOString().split("T")[0],
   }));
 
