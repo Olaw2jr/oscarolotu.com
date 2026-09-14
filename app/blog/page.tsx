485cba8b75987248aea01b1ad885bbba3909a43c
Oscar Olotu <oscar.eugine@gmail.com>
2026-09-14 21:59:05 +0300

    feat(blog): hide future scheduled posts

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,17 +2,24 @@ import type { Metadata } from "next";
 import { allPosts } from "contentlayer/generated";
 import PostCard from "./components/post-card";
 
+export const revalidate = 300;
+
 export const metadata: Metadata = {
   title: "Blog",
   description:
-    "All of my long-form thoughts on security, programming, leadership, product design, and more, collected in chronological order.",
+    "Makala kuhusu usalama, teknolojia, uongozi, vitabu, falsafa na maisha, pamoja na maandishi ya muda mrefu ya Oscar Olotu.",
 };
 
 export default async function Blog() {
+  const now = new Date();
+  const publishedPosts = allPosts.filter(
+    (post) => new Date(post.publishedAt) <= now
+  );
+
   return (
     <div className="mx-5 sm:mx-auto mt-16 sm:mt-20 md:border-l md:border-zinc-100 md:pl-6">
       <div className="flex max-w-3xl flex-col space-y-16">
-        {allPosts
+        {publishedPosts
           .sort((a, b) => {
             if (new Date(a.publishedAt) > new Date(b.publishedAt)) {
               return -1;
